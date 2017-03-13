66eb3e04375c193324677d4dfe24636067703b84
zxqdx <zxq001zxq001@gmail.com>
2017-03-13 18:26:09 -0400

    wip2

--- asset/player.js
+++ asset/player.js
@@ -1,104 +1,133 @@
 var Player = Player || {
   background: 'green',
   current: null,
-  currentTime: 0,
-  currentIndex: -1,
-  currentLock: false,
+  animation: {
+    time: 0,
+    index: {},
+    lock: false,
+    intervalId: -1
+  },
   constant: {
     /* Controls the internal refresh rate */
     INTERNAL_INTERVAL: 100,
     /* Controls the duration of additional info */
     ADD_INFO_DURATION: 2000,
     /* Estimation of the duration of last line of lyric */
-    LYRIC_LAST_LINE_DURATION: 5000
+    LYRIC_LAST_LINE_DURATION: 5000,
+    /* Supported lyric types */
+    SUPPORTED_LYRIC_TYPES: ['rLyric', 'tLyric'],
   },
-  interval: -1,
-  reset: () => {
-    if (Player.interval != -1) {
-      clearInterval(Player.interval);
+  init() {
+    this.index = {};
+    this.constant.SUPPORTED_LYRIC_TYPES.forEach(TYPE => {
+      this.animation.index[TYPE] = -1;
+      $('#stage span#' + TYPE).html('');
+    }, this);
+    this.animation.time = 0;
+    this.animation.lock = false;
+  },
+  reset() {
+    this.stop();
+    this.init();
+  },
+  stop() {
+    if (this.animation.intervalId != -1) {
+      clearInterval(this.animation.intervalId);
+      this.animation.intervalId = -1;
     }
-    Player.interval = -1;
-    Player.currentTime = 0;
-    Player.currentIndex = -1;
-    Player.currentLock = false;
-    $('#stage div').html('');
   },
-  play: () => {
-    if (Player.current === null) {
+  play() {
+    if (this.current === null) {
       return;
     }
 
     // Initializes lyric.
-    if (Player.current.lyric[0].time > 0) {
-      Player.current.lyric.unshift({
+    if (this.current.lyric[0].time > 0) {
+      this.current.lyric.unshift({
         time: 0,
         content: ''
       });
     }
 
     // DONE: Adds info.
-    var totalInfoCount = Player.current.addInfo.length;
+    var totalInfoCount = this.current.addInfo.length;
     var currentInfoIndex = 0;
     var rLyric = [];
-    for (var i = 0, len = Player.current.length; i < len; i++) {
-      rLyric.push(Player.current[i]);
+    for (var i = 0, len = this.current.lyric.length; i < len; i++) {
+      rLyric.push(this.current.lyric[i]);
       // Adds carryover info to the end of lyric.
       if (i == len - 1) {
-        var currentTime = Player.current.lyric[i].time + Player.constant.LYRIC_LAST_LINE_DURATION;
+        var currentTime = this.current.lyric[i].time + this.constant.LYRIC_LAST_LINE_DURATION;
         while (currentInfoIndex < totalInfoCount) {
           rLyric.push({
-            content: Player.current.addInfo[currentInfoIndex],
+            content: this.current.addInfo[currentInfoIndex],
             time: currentTime
           });
           currentInfoIndex++;
-          currentTime += Player.constant.ADD_INFO_DURATION;
+          currentTime += this.constant.ADD_INFO_DURATION;
         }
-        break loopI;
+        break;
       }
       // Attemps to sneak in info for empty lyrics whose durations are long enough.
-      if (Player.current.lyric[i].content) {
+      if (this.current.lyric[i].content) {
         continue;
       }
       var k = i + 1;
-      while (!(Player.current.lyric[k].content)) { // Combines consecutive empty lyrics.
+      while (!(this.current.lyric[k].content)) { // Combines consecutive empty lyrics.
         k++;
       }
-      var duration = Player.current.lyric[i + k].time - Player.current.lyric[i].time;
-      var infoCount = Math.floor(duration / Player.constant.ADD_INFO_DURATION);
+      var duration = this.current.lyric[i + k].time - this.current.lyric[i].time;
+      var infoCount = Math.floor(duration / this.constant.ADD_INFO_DURATION);
       for (var j = 0; j < infoCount; j++) {
         if (currentInfoIndex >= totalInfoCount) {
           break;
         }
         rLyric.push({
-          content: Player.current.addInfo[currentInfoIndex],
-          time: Player.current.lyric[i].time + Player.constant.ADD_INFO_DURATION * j;
-        })
+          content: this.current.addInfo[currentInfoIndex],
+          time: this.current.lyric[i].time + this.constant.ADD_INFO_DURATION * j
+        });
         currentInfoIndex++;
       }
       i = k - 1;
     }
+    if (rLyric[0].time === 0 && !(rLyric[0].content)) {
+      rLyric.splice(0, 1);
+    }
+    this.current.rLyric = rLyric;
 
-    // Resets player.
-    Player.reset();
+    // Resets this.
+    this.reset();
 
     // TODO: Starts animation.
-    Player.interval = window.setInterval(() => {
-      if (Player.currentLock) {
+    return;
+    this.interval = window.setInterval(() => {
+      this.animation.time += this.constant.INTERNAL_INTERVAL;
+      if (this.animation.lock) {
         return;
       }
-      Player.currentLock = true;
-      Player.currentTime += Player.constant.INTERNAL_INTERVAL;
-      var nextIndex = Player.currentIndex + 1;
-      while (nextIndex < Player.current.length && Player.current[nextIndex] <= Player.currentTime) {
-        nextIndex++;
-      }
+      this.animation.lock = true;
       
+      this.constant.SUPPORTED_LYRIC_TYPES.forEach(TYPE => {
+        let currentIndex = this.animation.index[TYPE];
+        let nextIndex = currentIndex + 1;
+        let current = this.current[TYPE];
+        while (nextIndex < current.length && current[nextIndex].time <= this.currentTime) {
+          nextIndex++;
+        }
+        nextIndex--;
+        if (currentIndex === nextIndex) {
+          // No new lyric. Exit.
+          return;
+        }
+        
+        
+      }, this);
 
-      Player.currentLock = false;
-    }, Player.constant.INTERNAL_INTERVAL);
+      this.animation.lock = false;
+    }, this.constant.INTERNAL_INTERVAL);
   },
-  setCurrent: json => {
-    Player.current = json;
+  setCurrent(json) {
+    this.current = json;
   },
 };
 
@@ -106,7 +135,7 @@ $(document).ready(() => {
   $('#btn-play').click(() => {
     var title = $('#song-title').val().trim();
     if (title !== '') {
-      Player.current.addInfo.unshift();
+      Player.current.addInfo.unshift("歌名：" + title);
     }
     Player.play();
   });
